8f4482049e1c4f4706cfa7b37504b40b68ccca16
kefan <likefan2009@sina.com>
2020-05-07 03:28:24 -0400

    Make IntroSection a shared component

--- src/BFS/BFS.tsx
+++ src/BFS/BFS.tsx
@@ -1,19 +1,11 @@
-import React, { FunctionComponent, useState, useContext, useRef } from "react";
-import { Elevation } from "@rmwc/elevation";
-import { ThemeProvider } from "@rmwc/theme";
-import { Button } from "@rmwc/button";
-import SmoothCollapse from "react-smooth-collapse";
-import { IntroExpandedContext } from "../Context/IntroExpandedContext";
+import React, { FunctionComponent, useState, useRef } from "react";
 import AddNodeButton from "./AddNodeButton"
 import { Stage, Layer, Circle } from "react-konva";
 import Konva from 'konva';
+import IntroSection from '../shared/IntroSection/IntroSection';
 
-import '@rmwc/theme/styles';
 import '@rmwc/fab/styles';
-import '@rmwc/icon/styles';
 import '@rmwc/tooltip/styles';
-import '@rmwc/button/styles';
-import '@rmwc/elevation/styles';
 import './BFS.css'
 
 const getRandomInt = (max: number) => {
@@ -31,43 +23,14 @@ interface nodeListStateInterface {
 }
 
 const BFS: FunctionComponent = () => {
-    const IntroExpanded = useContext(IntroExpandedContext);
-
-    const isOpenedGlobal = IntroExpanded && IntroExpanded.pagesExpanded[0];
-    const setIsOpenedGlobal = IntroExpanded && IntroExpanded.setExpanded;
-
     const [nodeListState, setNodeListState] = useState<nodeListStateInterface[]>([]);
-    const [isOpened, setIsOpened] = useState(isOpenedGlobal ? true : false);
-    const [collapsedTitleState, setCollapsedTitleState] = useState(isOpenedGlobal ? "" : " intro-section-collapsed");
-    const [expandButtonIsMounted, setExpandButtonIsMounted] = useState(isOpenedGlobal ? false : true);
-
     const nodeRef = useRef() as React.MutableRefObject<Konva.Circle>;
-
-    const collapseHandler = () => {
-        setIsOpenedGlobal && setIsOpenedGlobal(0);
-
-        setIsOpened((prevState: boolean) => {
-            return !prevState;
-        });
-
-        if (isOpened) {
-            setTimeout(() => {
-                setCollapsedTitleState(" intro-section-collapsed");
-                setExpandButtonIsMounted(true);
-            }, 150);
-        } else {
-            setExpandButtonIsMounted(false);
-            setTimeout(() => {
-                setCollapsedTitleState("");
-            }, 20);
-        }
-    }
 
     const addNodeHandler = (x: number, y: number) => {
         setNodeListState(prevState => {
             return [...prevState, {
                 value: getRandomInt(100),
-                elevation: 3,
+                elevation: 5,
                 className: "",
                 xPosition: x,
                 yPosition: y,
@@ -91,7 +54,7 @@ const BFS: FunctionComponent = () => {
 
     const mouseOutHandler = (index: number) => {
         nodeRef.current.to({
-            shadowBlur: 10,
+            shadowBlur: 5,
             duration: 0.15
         });
         const newNodeState = [...nodeListState];
@@ -125,54 +88,15 @@ const BFS: FunctionComponent = () => {
 
     return (
         <div>
-            <div className={"intro-section" + collapsedTitleState}>
-                <div className="intro-section-content">
-                    <div className={"intro-section-content-title" + collapsedTitleState}>
-                        <h1>Breadth-First Search</h1>
-                    </div>
-
-
-                    <SmoothCollapse expanded={isOpened}>
-                        <div className="description">
-                            <p>
-                                "Breadth-first search (BFS) is an algorithm for traversing or searching tree or graph data structures. It starts at the tree root (or some arbitrary node of a graph,
-                                sometimes referred to as a 'search key'), and explores all of the neighbor nodes at the present depth prior to moving on to the nodes at the next depth level.
-                                It uses the opposite strategy as depth-first search, which instead explores the node branch as far as possible before being forced to backtrack and expand other nodes."
-                            </p>
-
-                            <p>--Wikipedia</p>
-                        </div>
-
-                        {isOpened &&
-                            <div className='collapse-button'>
-                                <ThemeProvider
-                                    options={{
-                                        primary: 'white'
-                                    }} >
-                                    <Button onClick={collapseHandler} label="start exploring" unelevated />
-                                </ThemeProvider>
-                            </div>
-                        }
-                    </SmoothCollapse>
-
-                </div>
-
-                {expandButtonIsMounted &&
-                    <div className='expand-button'>
-                        <ThemeProvider
-                            options={{
-                                primary: 'white'
-                            }} >
-                            <Button onClick={collapseHandler} label="expand" dense />
-                        </ThemeProvider>
-                    </div>
-                }
-
-            </div>
+            <IntroSection title="Breadth-First Search" source='Wikipedia'>
+                "Breadth-first search (BFS) is an algorithm for traversing or searching tree or graph data structures. It starts at the tree root (or some arbitrary node of a graph,
+                sometimes referred to as a 'search key'), and explores all of the neighbor nodes at the present depth prior to moving on to the nodes at the next depth level.
+                It uses the opposite strategy as depth-first search, which instead explores the node branch as far as possible before being forced to backtrack and expand other nodes."
+            </IntroSection>
 
             <div className="operation-section">
                 <div className="operation-node-section">
-                    <Stage width={window.innerWidth - 850} height={window.innerHeight}>
+                    <Stage width={window.innerWidth - 580} height={window.innerHeight}>
                         <Layer>
                             {nodeListState.map((node, index) => {
                                 return (
@@ -203,7 +127,7 @@ const BFS: FunctionComponent = () => {
 
                 <div className="add-node-button">
                     <AddNodeButton onClick={() => {
-                        addNodeHandler(Math.random() * (window.innerWidth - 950) + 35, Math.random() * 500 + 30)
+                        addNodeHandler(Math.random() * (window.innerWidth - 300) + 100, Math.random() * 500 + 30)
                     }} />
                 </div>
             </div>
