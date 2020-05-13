324f92cdec8796c0ca36926070a121df6d920ca3
jurgen <tigeryhw@gmail.com>
2020-05-13 13:08:57 +0800

    fifth try

--- src/Context/CurrentPageContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-export interface CurrentPageInterface {
-    currentPage: number,
-    setCurrentPage: (pageNumber: number) => void
-}
-
-export const CurrentPageContext = createContext<CurrentPageInterface | null>(null);
--- src/Context/IntroExpandedContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from "react";
-
-export interface IntroExpandedInterface {
-    pagesExpanded: boolean[],
-    setExpanded: (index: number) => void
-};
-
-export const IntroExpandedContext = createContext<IntroExpandedInterface | null>(null);
