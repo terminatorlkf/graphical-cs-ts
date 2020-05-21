a9bc73e91cd19294f153fc83d4ef837009b9918b
jurgen <tigeryhw@gmail.com>
2020-05-21 15:39:28 +0800

    add homepage background

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,10 +1,19 @@
-import React, { FunctionComponent} from "react";
+import React, { FunctionComponent } from "react";
+import HomeBackground from "./HomeBackground";
+
+import './Home.css';
 
 const Home: FunctionComponent = () => {
     return (
-        <div>
-            Nothing
-        </div>
+        <React.Fragment>
+            <HomeBackground />
+            <div>
+                <h1 className="title-welcome"> Welcome </h1>
+                <h2 className='title-secondary'>
+                    We're committed to building technology that helps people find ways to be together.
+            </h2>
+            </div>
+        </React.Fragment>
     )
 }
 
---
+++ src/Home/HomeBackground.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { Circle, Layer, Stage, Line } from "react-konva";
+import { useSprings, animated, config } from '@react-spring/konva';
+import { KonvaEventObject } from "konva/types/Node";
+
+import './HomeBackground.css';
+
+export default function HomeBackground() {
+
+    const presetState = [...Array(32)].map((_, index) => {
+        const block = Math.floor(index / 8);
+        const blokcWidth = window.innerWidth * (1 / 2);
+        const blokcHeight = window.innerHeight * (1 / 2);
+        if (Number.isInteger(index / 8)) {
+            return ({
+                key: index,
+                xPosition: block % 2 * blokcWidth + blokcWidth * 0.5,
+                yPosition: Math.floor(block / 2) * blokcHeight + blokcHeight * 0.5
+            });
+        }
+        return ({
+            key: index,
+            xPosition: block % 2 * blokcWidth + Math.random() * blokcWidth,
+            yPosition: Math.floor(block / 2) * blokcHeight + Math.random() * blokcHeight
+        });
+    });
+
+    const animations = useSprings(presetState.length, presetState.map(state => ({
+        to: async next => {
+            while (1) {
+                await next({ x: state.xPosition, y: state.yPosition })
+                await next({ x: state.xPosition + 0, y: state.yPosition + Math.random() * 100 })
+                await next({ x: state.xPosition + Math.random() * 100, y: state.yPosition + 0 })
+                await next({ x: state.xPosition + 0, y: state.yPosition + 0 })
+            }
+        },
+        from: { x: state.xPosition, y: state.yPosition }
+    })));
+
+    return (
+        <div className="home-background">
+            <Stage width={window.innerWidth} height={window.innerHeight}>
+                <Layer>
+                    {presetState.slice(0, 8).map((state, index) => {
+                        const props = animations[index]
+                        return (
+                            <React.Fragment>
+                                <animated.Group
+                                    {...props}
+                                >
+                                    <Line
+                                        points={[
+                                            0, 
+                                            0, 
+                                            animations[0].x.getValue() - props.x.getValue(), 
+                                            animations[0].y.getValue() - props.y.getValue()]}
+                                        stroke='black' />
+                                    <Circle radius={5} fill="black" />
+                                </animated.Group>
+                            </React.Fragment>
+                        );
+                    })}
+                </Layer>
+            </Stage>
+        </div>
+    );
+}
