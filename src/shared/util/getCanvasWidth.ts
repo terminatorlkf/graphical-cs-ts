a25636f8294441aa7a474c38845a76aa4d11c9ac
terminatorlkf <likefan2009@sina.com>
2020-08-13 22:35:12 -0400

    Optimizing the canvas size in resize event

--- src/shared/util/getCanvasWidth.ts
+++ src/shared/util/getCanvasWidth.ts
@@ -1,9 +1,11 @@
 export const getCanvasWidth = () => {
-    let newCanvasWidth: number = 0;
-    if (window.innerWidth < 1700) newCanvasWidth = window.innerWidth * 1.2 / 3;
-    else if (window.innerWidth < 2000) newCanvasWidth = window.innerWidth * 1.75 / 3;
-    else if (window.innerWidth < 2500) newCanvasWidth = window.innerWidth * 2 / 3;
-    else newCanvasWidth = window.innerWidth * 1 / 3;
+    let canvasWidth: number = 0;
+    if (window.innerWidth < 1300) canvasWidth = window.innerWidth * 1 / 3;
+    else if (window.innerWidth < 1500) canvasWidth = window.innerWidth * 1.23 / 3;
+    else if (window.innerWidth < 1700) canvasWidth = window.innerWidth * 1.45 / 3;
+    else if (window.innerWidth < 2000) canvasWidth = window.innerWidth * 1.75 / 3;
+    else if (window.innerWidth < 2500) canvasWidth = window.innerWidth * 2 / 3;
+    else canvasWidth = window.innerWidth * 1 / 3;
 
-    return newCanvasWidth;
+    return canvasWidth;
 }
